9703ae6773115801d6c364f473b80145eae7bd21
Stefan Goste <st.goste@gmx.de>
2024-08-12 21:54:17 +0200

    Chapter 2 finished

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,76 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: usize,
+    unit_price: usize,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: usize, unit_price: usize) -> Self {
+        Self::check_product_name(&product_name);
+        Self::check_quantity(&quantity);
+        Self::check_unit_price(&unit_price);
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    fn check_product_name(product_name: &str) {
+        if product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+
+        if product_name.len() > 300 {
+            panic!("Product name  cannot be longer than 300 bytes");
+        }
+    }
+
+    fn check_quantity(quantity: &usize) {
+        if quantity == &0 {
+            panic!("Quantity cannot be zero");
+        }
+    }
+
+    fn check_unit_price(unit_price: &usize) {
+        if unit_price == &0 {
+            panic!("Unit price cannot be zero");
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &usize {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &usize {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> usize {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        Self::check_product_name(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: usize) {
+        Self::check_quantity(&quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: usize) {
+        Self::check_unit_price(&unit_price);
+        self.unit_price = unit_price;
+    }
+}
