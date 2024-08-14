25e4f7a54805cb65aeabbc36ff1be52a8d4f4643
Stefan Goste <st.goste@gmx.de>
2024-08-14 23:02:28 +0200

    Chapter 5 Part A

--- exercises/05_ticket_v2/04_if_let/src/lib.rs
+++ exercises/05_ticket_v2/04_if_let/src/lib.rs
@@ -8,7 +8,15 @@ impl Shape {
     // TODO: Implement the `radius` method using
     //  either an `if let` or a `let/else`.
     pub fn radius(&self) -> f64 {
-        todo!()
+        // let Shape::Circle { radius } = &self else {
+        //     panic!()
+        // };
+        // *radius
+        if let Shape::Circle { radius } = &self {
+            *radius
+        } else {
+            panic!()
+        }
     }
 }
 
