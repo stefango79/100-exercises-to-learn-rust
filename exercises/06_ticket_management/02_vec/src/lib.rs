b5a2cfe392837fc76b763f82838d2ce0523b1672
Stefan Goste <st.goste@gmx.de>
2024-08-17 23:01:22 +0200

    Chapter 6 Part B

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,17 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let n = n as usize;
+    let mut memo = Vec::new();
+
+    memo.push(0);
+    memo.push(1);
+
+    for i in 2..=n {
+        memo.push(memo[i - 1] + memo[i - 2]);
+    }
+
+    memo[n]
 }
 
 #[cfg(test)]
